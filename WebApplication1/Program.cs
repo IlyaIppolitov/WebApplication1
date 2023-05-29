5c59745c9d81574381392c17f14f3db9dd25f964
IppolitovIS <IppolitovIS@gmail.com>
2023-05-29 21:45:22 +0300

    DaysTillNewYear calculated

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,6 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+// builder - паттерн проектирование Строитель (Builder)
 
-app.MapGet("/", () => "Hello World!");
+var app = builder.Build();
+var curDate = DateTime.Now.ToString();
+
+// app.MapGet("/", () => "Hello World!");
+// app.MapGet("/", () => DateTime.Now.ToString());
+app.MapGet("/new_year", getDaysTillNewYear);
 
 app.Run();
+
+string getDaysTillNewYear()
+{
+    /// Текущий год
+    var year = DateTime.Now.Year;
+    /// Текущая дата
+    var newYear = new DateTime(year, 12, 31);
+    /// Расчёт количества дней до ноговго года
+    var days = (newYear - DateTime.Now).Days;
+
+    return $"До нового года осталось {days} дней!";
+}
