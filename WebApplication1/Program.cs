42e0172442fe599541f773f94a021726189fa8ab
IppolitovIS <IppolitovIS@gmail.com>
2023-06-05 20:30:51 +0300

    Type of price changed to decimal

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -3,13 +3,25 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 var curDate = DateTime.Now.ToString();
-
-// app.MapGet("/", () => "Hello World!");
-// app.MapGet("/", () => DateTime.Now.ToString());
 // app.MapGet("/new_year", getDaysTillNewYear);
-app.MapGet("/customs_duty", (double? price) => (getCustomsDuty(price)));
+app.MapGet("/customs_duty", (decimal? price) => (getCustomsDuty(price)));
 
 app.Run();
+
+string getCustomsDuty(decimal? price)
+{
+    if (price is null)
+        return "Не передана цена";
+    
+    if (price < 0)
+        return "Вы ввели некоретную цену товара!";
+
+    if (price <= 200)
+        return "Пошлина отсутствует!\nТовары до 200 Евро полиной не облагаются!";
+
+    decimal? duty = (price - 200m) * 0.15m;
+    return $"{duty} Евро - таможенная пошлина.";
+}
 
 string getDaysTillNewYear()
 {
@@ -21,16 +33,4 @@ string getDaysTillNewYear()
     var days = (newYear - DateTime.Now).Days;
 
     return $"До нового года осталось {days} дней!";
-}
-
-string getCustomsDuty(double? price)
-{
-    if (price < 0)
-        return "Вы ввели некоретную цену товара!";
-
-    if (price <= 200)
-        return "Пошлина отсутствует!\nТовары до 200 Евро полиной не облагаются!";
-
-    var duty = (price - 200) * 0.15;
-    return $"{duty} Евро - таможенная пошлина.";
 }
